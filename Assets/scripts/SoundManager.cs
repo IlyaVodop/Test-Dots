5f9d83cb8043acc0f8a91d5f9201eb4c8ec01ec4
Ilya <ilyavodop1@gmail.com>
2021-03-11 17:50:16 +0500

    Create SoundManager.cs and UIManager.cs

---
+++ Assets/scripts/SoundManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager Instance = null;
+
+
+    public AudioClip BtnClip;
+
+    public AudioClip SoundClip;
+
+    void OnEnable()
+    {
+
+        PlayAudio(SoundClip);
+    }
+    public void PlayAudio(AudioClip clip)
+    {
+        GetComponent<AudioSource>().PlayOneShot(clip);
+    }
+
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+    }
+
+    void OnDestroy()
+    {
+        Instance = null;
+    }
+
+}
---
+++ Assets/scripts/UIManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    private UIState _currentUIState;
+
+    [SerializeField] private GameObject _mainPanel;
+
+    [SerializeField] private GameObject _level;
+
+    [SerializeField] private Button _startGame;
+
+    [SerializeField] private Button _exitGame;
+
+    void Start()
+    {
+        Subscribe();
+    }
+
+    private void UpdateState()
+    {
+        _mainPanel.SetActive(_currentUIState == UIState.Game);
+    }
+
+    public enum UIState
+    {
+        Game
+    }
+
+    public void SetState(UIState state)
+    {
+        _currentUIState = state;
+        UpdateState();
+    }
+
+    private void Subscribe()
+    {
+        _startGame.onClick.AddListener(() =>
+        {
+            SoundManager.Instance.PlayAudio(SoundManager.Instance.BtnClip);
+            _mainPanel.gameObject.SetActive(false);
+            _level.gameObject.SetActive(true);
+        });
+
+        _exitGame.onClick.AddListener(() => { SoundManager.Instance.PlayAudio(SoundManager.Instance.BtnClip); });
+    }
+
+    void Update()
+    {
+
+    }
+}
